d9bd06d2fca60f9761545fcb7fc0d22bd0bf6f34
Samuel <50973822+slastrina@users.noreply.github.com>
2021-11-19 17:12:47 +1100

    working query sample

--- oracle_spatial_queries/retrieve_points_within_polygon.py
+++ oracle_spatial_queries/retrieve_points_within_polygon.py
@@ -38,27 +38,30 @@ connection = cx_Oracle.connect(user=os.getenv('DB_USER', None), password=os.gete
 
 cursor = connection.cursor()
 
-# # using a pandas dataframe
-# cursor.execute("""select NBN_LOCATION_PID, lat, lng
-#                     from (select l.nbn_location_pid NBN_LOCATION_PID, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.y lat, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.x lng from location l
-#                     where rownum < 100000)""")
-# start = timer()
-# gdf = gpd.GeoDataFrame(cursor.fetchall(), columns=['NBN_LOCATION_PID', 'lat', 'lng'])
-# end = timer()
-# print(end - start, len(gdf))
-#
-# # using fetchmany on the cursor and managing the chunks in python
-# locations = []
-# cursor.execute("""select NBN_LOCATION_PID, lat, lng
-#                     from (select l.nbn_location_pid NBN_LOCATION_PID, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.y lat, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.x lng from location l
-#                     where rownum < 100000)""")
-# start = timer()
-# for i, chunk in enumerate(chunks(cursor)):
-#     for row in chunk:
-#         locations.append(row)
-# end = timer()
-# print(end - start, len(locations))
+##################################################################
+# using a pandas dataframe
+cursor.execute("""select NBN_LOCATION_PID, lat, lng
+                    from (select l.nbn_location_pid NBN_LOCATION_PID, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.y lat, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.x lng from location l
+                    where rownum < 100000)""")
+start = timer()
+gdf = gpd.GeoDataFrame(cursor.fetchall(), columns=['NBN_LOCATION_PID', 'lat', 'lng'])
+end = timer()
+print(end - start, len(gdf))
 
+##################################################################
+# using fetchmany on the cursor and managing the chunks in python
+locations = []
+cursor.execute("""select NBN_LOCATION_PID, lat, lng
+                    from (select l.nbn_location_pid NBN_LOCATION_PID, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.y lat, SDO_CS.TRANSFORM(l.search_point,4283).sdo_point.x lng from location l
+                    where rownum < 100000)""")
+start = timer()
+for i, chunk in enumerate(chunks(cursor)):
+    for row in chunk:
+        locations.append(row)
+end = timer()
+print(end - start, len(locations))
+
+##################################################################
 # query using actual input geometry
 for f in polygon_details['features']:
     poly = shape(f["geometry"])
